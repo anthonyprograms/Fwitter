c84bb9c8f533d5e78cf269824819088dfeb2f955
Anthony <anthonygreen274@icloud.com>
2015-08-16 14:43:25 -0700

    Connected websockets

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(require('./controllers/api/posts'))
 app.use('/api/sessions', require('./controllers/api/sessions'))
 app.use('/api/users', require('./controllers/api/users'))
 
-app.listen(3000, function () {
-    console.log('Server listening on', 3000);
-});
+var server = app.listen(3000, function () {
+    console.log('Server listening on', 3000)
+})
+require('./websockets').connect(server)
