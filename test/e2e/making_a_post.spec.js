c710b49cede1fac4570fd1df335d8375c9fd02ce
anthonyprograms <anthony.williams@student.csulb.edu>
2015-08-17 12:30:29 -0700

    Making a post with protractor

--- test/e2e/making_a_post.spec.js
+++ test/e2e/making_a_post.spec.js
@@ -2,10 +2,22 @@ describe('making a post', function () {
     it('logs in and creates a new post', function () {
         // Go to homepage
         browser.get('http://localhost:3001')
+
         // Click 'login
+        element(by.css('nav .login')).click()
+
         // Fill out and submit login form
+        element(by.model('username')).sendKeys('anthony')
+        element(by.model('password')).sendKeys('pass')
+        element(by.css('form .btn')).click()
+
         // Submit a new post on the posts page
+        var post = 'my new post'
+        element(by.model('postBody')).sendKeys(post)
+        element(by.css('form .btn')).click()
 
         // The user should now see their post as the first post on the page
+
+
     })
 })
