f1fbbfb05826d55298553aec792f0852506eae40
anthonyprograms <anthony.williams@student.csulb.edu>
2015-08-16 19:37:45 -0700

    Protractor testing

---
+++ test/e2e/making_a_post.spec.js
@@ -0,0 +1,11 @@
+describe('making a post', function () {
+    it('logs in and creates a new post', function () {
+        // Go to homepage
+        browser.get('http://localhost:3000')
+        // Click 'login
+        // Fill out and submit login form
+        // Submit a new post on the posts page
+
+        // The user should now see their post as the first post on the page
+    })
+})
