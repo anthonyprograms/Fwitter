95e910e5b6cfccde9b978ad7fb49ea42533fdccc
anthonyprograms <anthony.williams@student.csulb.edu>
2015-08-16 17:30:06 -0700

    Dynamic websocket hostname

--- ng/websockets.js
+++ ng/websockets.js
@@ -1,15 +1,25 @@
 angular.module('app')
-.run(function ($rootScope, $timeout) {
-    (function connect() {
-        var url = 'ws://localhost:3000'
-        var connection = new WebSocket(url)
-        connection.onclose = function (e) {
-            console.log('WebSocket closed. Reconnecting...')
-            $timeout(connect, 10*1000)
+.service('WebSocketSvc', function ($rootScope) {
+    function websocketHost() {
+        if ($window.location.protocol === "https:") {
+            return "wss://" + window.location.host
+        } else {
+            return "ws://" + window.location.host
         }
+    }
+
+    var connection
+    this.connect = function () {
+        connection = new WebSocket(websocketHost())
         connection.onmessage = function (e) {
             var payload = JSON.parse(e.data)
             $rootScope.$broadcast('ws:' + payload.topic, payload.data)
         }
-    })()
+    }
+    this.send = function (topic, data) {
+        var json = JSON.strigify({topic: topic, data: data})
+        connection.send(json)
+    }
+}).run(function (WebSocketSvc) {
+    WebSocketSvc.connect()
 })
