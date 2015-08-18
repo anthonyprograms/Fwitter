7b0ecd792396ef2afd9780d540924ddd2e7a50f3
anthonyprograms <anthony.williams@student.csulb.edu>
2015-08-17 18:51:08 -0700

    Test POST with authentication

--- test/server/controllers/api/posts.spec.js
+++ test/server/controllers/api/posts.spec.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect
 var api = require('../../support/api')
+var user = require('../../support/user')
 var Post = require('../../../../models/post')
 
 describe('controllers.api.posts', function () {
@@ -15,7 +16,6 @@ describe('controllers.api.posts', function () {
             ]
             Post.create(posts, done)
         })
-
         it('has 3 posts', function (done) {
             api.get('/api/posts')
             .expect(200)
@@ -23,6 +23,30 @@ describe('controllers.api.posts', function () {
                 expect(response.body).to.have.length(3)
             })
             .end(done)
+        })
+    })
+
+    describe('POST /api/posts', function () {
+        var token
+        beforeEach(function (done) {
+            user.create('anthony', 'pass', function (err, user) {
+                token = user.token
+                done(err)
+            })
+        })
+
+        beforeEach(function (done) {
+            api.post('/api/posts')
+            .send({body: 'this is my new post'})
+            .set('X-Auth', token)
+            .expect(201)
+            .end(done)
+        })
+        it('added 1 new post', function (done) {
+            Post.findOne(function (err, post) {
+                expect(post.body).to.equal('this is my new post')
+                done(err)
+            })
         })
     })
 })
