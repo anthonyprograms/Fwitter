24276958004920f2af1cfc6bf43a33f19f27823c
anthonyprograms <anthony.williams@student.csulb.edu>
2015-08-17 18:32:59 -0700

    API GET test

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 router.use(bodyParser.json())
 
 router.use(require('../auth'))
-router.use('/api/posts', require('./api/posts'))
+router.use(require('./api/posts'))
 router.use('/api/sessions', require('./api/sessions'))
 router.use('/api/users', require('./api/users'))
 router.use(require('./static'))
--- test/server/controllers/api/posts.spec.js
+++ test/server/controllers/api/posts.spec.js
@@ -1,8 +1,28 @@
 var expect = require('chai').expect
-var ctrl = require('../../../../controllers/api/posts')
+var api = require('../../support/api')
+var Post = require('../../../../models/post')
 
 describe('controllers.api.posts', function () {
-    it ('exists', function () {
-        expect(ctrl).to.exist
+    beforeEach(function (done) {
+        Post.remove({}, done)
+    })
+    describe('GET /api/posts', function () {
+        beforeEach(function (done) {
+            var posts = [
+                {body: 'post1', username: 'anthony'},
+                {body: 'post2', username: 'anthony'},
+                {body: 'post3', username: 'anthony'}
+            ]
+            Post.create(posts, done)
+        })
+
+        it('has 3 posts', function (done) {
+            api.get('/api/posts')
+            .expect(200)
+            .expect(function (response) {
+                expect(response.body).to.have.length(3)
+            })
+            .end(done)
+        })
     })
 })
