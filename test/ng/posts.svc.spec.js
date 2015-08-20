8ce22118d548a10c1948be32810d30aa6285074e
anthonyprograms <anthony.williams@student.csulb.edu>
2015-08-20 09:34:50 -0700

    Testing Spies

--- test/ng/posts.svc.spec.js
+++ test/ng/posts.svc.spec.js
@@ -42,6 +42,11 @@ describe('posts.ctrl', function () {
             ])
             return deferred.promise
         }
+        mocksPostsSvc.create = function () {
+            var deferred = $q.defer()
+            deferred.resolve()
+            return deferred.promise
+        }
     }))
 
     beforeEach(inject(function ($rootScope, $controller) {
@@ -55,5 +60,12 @@ describe('posts.ctrl', function () {
     it('loads posts from the service', function () {
         $scope.$digest()
         expect($scope.posts).to.have.length(2)
+    })
+
+    it('sends a new post to the service', function () {
+        sinon.spy(mockPostsSvc, 'create')
+        $scope.post = {body: 'my new post'}
+        $scope.addPost()
+        expect(mockPostsSvc.create).to.have.been.calledWith({body: 'my new post'})
     })
 })
